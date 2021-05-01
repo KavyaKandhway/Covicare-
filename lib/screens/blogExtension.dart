b10c1d00a3d522b6f6d3f99eea89cba4e28a79b7
KavyaKandhway <46575071+KavyaKandhway@users.noreply.github.com>
2021-05-01 16:31:07 +0530

    blog

--- lib/screens/blogExtension.dart
+++ lib/screens/blogExtension.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BlogExtend extends StatefulWidget {
@@ -8,54 +9,71 @@ class BlogExtend extends StatefulWidget {
 }
 
 class _BlogExtendState extends State<BlogExtend> {
+  int index;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    "Past Experience",
-                    style: TextStyle(
-                      fontSize: 30.0,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Image(
-                    image: AssetImage('images/sample.jpg'),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    "There is nothing to worry much about it. All you need to do is to take necessary precautuons for yourself and for your family and carefully follow the prescribe routine as suggested by certified medical practioners. Keeping oneself calm under the situation is important. Ask infected person to wear a mask at home all times while home isolation.There is nothing to worry much about it. All you need to do is to take necessary precautuons for yourself and for your family and carefully follow the prescribe routine as suggested by certified medical practioners. Keeping oneself calm under the situation is important. Ask infected person to wear a mask at home all times while home isolation.",
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      "-XYZ",
-                      textAlign: TextAlign.right,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Blog"),
+          backgroundColor: Colors.cyan[500],
         ),
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('blog').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView(
+                children: snapshot.data.docs.map((document) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            document['title'],
+                            style: TextStyle(
+                              fontSize: 30.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Image(
+                            image: AssetImage('images/sample.jpg'),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            document['content'],
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            child: Text(
+                              document['name'],
+                              textAlign: TextAlign.right,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+            }),
       ),
     );
   }
--- lib/screens/blogs.dart
+++ lib/screens/blogs.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:covicare/screens/blogExtension.dart';
 import 'package:covicare/screens/blogForm.dart';
 import 'package:flutter/material.dart';
@@ -11,128 +12,79 @@ class Blog extends StatefulWidget {
 class _BlogState extends State<Blog> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    style: TextStyle(height: 1.0),
-                    decoration: InputDecoration(
-                      counterText: "",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.zero,
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.zero,
-                        borderSide: BorderSide(color: Colors.grey, width: 2.0),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.zero,
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                      ),
-                      disabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.zero,
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                      ),
-                      labelText: 'Search',
-                    ),
-                  ),
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [
-                      BlogCard(
-                        title: "XYZ XYZ YXZ",
-                        author: "XYZ",
-                      ),
-                      BlogCard(
-                        title: "XYZ XYZ YXZ",
-                        author: "XYZ",
-                      ),
-                      BlogCard(
-                        title: "XYZ XYZ YXZ",
-                        author: "XYZ",
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )));
-  }
-}
-
-class BlogCard extends StatelessWidget {
-  BlogCard({
-    this.img,
-    this.title,
-    this.author,
-  });
-  final String img, title, author;
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => BlogExtend()),
-        );
-      },
-      child: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.purple[400],
-                width: 1.5,
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Center(
-                      child: Image(
-                        image: AssetImage('images/sample.jpg'),
-                      ),
-                    ),
-                    Container(
-                      height: 45,
-                      child: Row(
-                        children: [
-                          Text(
-                            "Past Experience",
-                            style: TextStyle(color: Colors.black, fontSize: 20),
-                            textAlign: TextAlign.left,
+    return Scaffold(
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('blog').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView(
+                children: snapshot.data.docs.map((document) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => BlogExtend()),
+                      );
+                    },
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple[400],
+                              width: 1.5,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
                           ),
-                          Text(
-                            "-XYZ",
-                            style:
-                                TextStyle(color: Colors.black54, fontSize: 10),
-                            textAlign: TextAlign.end,
-                          )
-                        ],
+                          child: Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Center(
+                                    child: Image(
+                                      image: AssetImage('images/sample.jpg'),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 45,
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          document['title'],
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 20),
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        Text(
+                                          "-" + document['name'],
+                                          style: TextStyle(
+                                              color: Colors.black54,
+                                              fontSize: 10),
+                                          textAlign: TextAlign.end,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ]),
+                          ),
+                        ),
                       ),
                     ),
-                  ]),
-            ),
-          ),
-        ),
-      ),
-    );
+                  );
+                }).toList(),
+              );
+            }));
   }
 }
