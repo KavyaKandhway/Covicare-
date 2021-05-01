2f06d25f5f16d7604e5b66ed04aa6ca76bbf8895
KavyaKandhway <46575071+KavyaKandhway@users.noreply.github.com>
2021-05-01 15:37:17 +0530

    supply display

--- lib/InsideHome/needsupplies.dart
+++ lib/InsideHome/needsupplies.dart
@@ -24,10 +24,172 @@ class _NeedSuppliesState extends State<NeedSupplies> {
             );
           }
           return ListView(
-            children: snapshot.data.documents.map((document) {
+            children: snapshot.data.docs.map((document) {
               return Center(
-                child: Container(
-                  child: Text("phone " + document['phone']),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 30,
+                    decoration: BoxDecoration(
+                      color: Colors.cyan[100],
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Text(
+                              document['supplyname'],
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            alignment: Alignment.topLeft,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            child: Text("Phone no. " + document['phone']),
+                            alignment: Alignment.topLeft,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: Text(
+                                "Items Available ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.left,
+                              )),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              document['oxygen']
+                                  ? Container(
+                                      child: Text("Oxygen "),
+                                      width: 70,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                    )
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                              document['bed']
+                                  ? Container(
+                                      child: Text("Bed "),
+                                      width: 50,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                    )
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                              document['rem']
+                                  ? Container(
+                                      child: Text("Remdesivir "),
+                                      width: 90,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                    )
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                              document['fav']
+                                  ? Container(
+                                      child: Text("Favipiravir "),
+                                      width: 100,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                    )
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              document['oxygen']
+                                  ? Container(
+                                      width: 70,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                      child: Text("Rs." +
+                                          document['oxyamt'].toString() +
+                                          " "))
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                              document['bed']
+                                  ? Container(
+                                      width: 50,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                      child: Text("Rs." +
+                                          document['bedamt'].toString() +
+                                          " "))
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                              document['rem']
+                                  ? Container(
+                                      width: 90,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                      child: Text("Rs." +
+                                          document['remamt'].toString() +
+                                          " "))
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                              document['fav']
+                                  ? Container(
+                                      width: 100,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 1,
+                                              color: Colors.cyan[500])),
+                                      child: Text("Rs." +
+                                          document['favamt'].toString() +
+                                          " "))
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               );
             }).toList(),
